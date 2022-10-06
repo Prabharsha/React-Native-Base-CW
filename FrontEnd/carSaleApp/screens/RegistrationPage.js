02398b9f5699aeebeef7bc1c331d79d918c227ba
Prabharsha <pansiluprabharsha1@gamil.com>
2022-10-06 11:47:50 +0530

    updated

--- FrontEnd/carSaleApp/screens/RegistrationPage.js
+++ FrontEnd/carSaleApp/screens/RegistrationPage.js
@@ -1,9 +1,26 @@
-import React, { Component,Text } from 'react'
+import { NativeBaseProvider, View ,Text } from 'native-base'
+import React from 'react'
+import { StyleSheet } from 'react-native'
 
-export default class RegistrationPage extends Component {
-  render() {
-    return (
-      <Text>RegistrationPage</Text>
-    )
-  }
+
+export default function RegistrationPage() {
+  return (
+    <NativeBaseProvider >
+        <View flex={1} backgroundColor={"black"}>
+        <View style={styles.container} >
+
+        </View>
+        </View>
+    </NativeBaseProvider>
+  )
+ 
 }
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:'#89f98d',
+    flex: 1,
+    marginTop: 0,
+    paddingLeft: 10,
+    color: '#05375a',
+  }
+})
